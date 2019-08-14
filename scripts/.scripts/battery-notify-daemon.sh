071d389549d008b81690c8fde3c5aa8d7f301a1a
Leonardo Idone <idone.leonardo@gmail.com>
2019-08-14 19:36:44 +0200

    Corrected errors and expanded bat-notify

--- scripts/.scripts/battery-notify-daemon.sh
+++ scripts/.scripts/battery-notify-daemon.sh
@@ -3,8 +3,12 @@
 # Battery notification daemon
 
 while true; do
-  if [ $(cat /sys/class/power_supply/BAT0/capacity) -le 15 && "$(cat /sys/class/power_supply/BAT0/status)" != "Charging" ] ; then
+  if [ $(cat /sys/class/power_supply/BAT0/capacity) -le 15 ] && [ "$(cat /sys/class/power_supply/BAT0/status)" != "Charging" ] ; then
     dunstify -t 5000 -i /usr/share/icons/Adwaita/48x48/legacy/battery-caution.png -u CRITICAL "WARNING" "Battery nearly empty"
+  else 
+    if [ $(cat /sys/class/power_supply/BAT0/capacity) -le 10 ] ; then
+      systemctl hibernate
+    fi
   fi
   sleep 5m
 done

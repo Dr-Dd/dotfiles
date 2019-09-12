e532e57211793e8cf0c73c15bf0feea238a7ce86
Leonardo Idone <idone.leonardo@gmail.com>
2019-09-12 14:43:38 +0200

    Fixed battery daemon

--- home-scripts/.scripts/battery-notify-daemon.sh
+++ home-scripts/.scripts/battery-notify-daemon.sh
@@ -5,9 +5,9 @@
 while true; do
     if [ $(cat /sys/class/power_supply/BAT0/capacity) -le 15 ] && [ "$(cat /sys/class/power_supply/BAT0/status)" != "Charging" ] ; then
         dunstify -t 0 -i /usr/share/icons/Adwaita/48x48/legacy/battery-caution.png -u CRITICAL "WARNING" "Battery nearly empty"
-    else if [ $(cat /sys/class/power_supply/BAT0/capacity) -le 10 ] && [ "$(cat /sys/class/power_supply/BAT0/status)" != "Charging" ] ; then
-             systemctl hibernate
-         fi
+        if [ $(cat /sys/class/power_supply/BAT0/capacity) -le 10 ] && [ "$(cat /sys/class/power_supply/BAT0/status)" != "Charging" ] ; then
+            systemctl hibernate
+        fi
     fi
     sleep 5m
 done

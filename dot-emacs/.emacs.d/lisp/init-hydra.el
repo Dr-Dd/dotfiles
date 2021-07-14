bdfe16d4401cc3dba34a25d50f56b36ca5fdcaa8
drd <idone.leonardo@gmail.com>
2021-07-14 14:03:57 +0200

    Removed submodule before reintegrating it

--- dot-emacs/.emacs.d/lisp/init-hydra.el
+++
@@ -1,9 +0,0 @@
-;;; package --- Summary
-;;; Commentary:
-;;; Hydra configs (used mostly for creating menus)
-;;; Code:
-
-(use-package hydra :ensure t )
-
-(provide 'init-hydra)
-;;; init-hydra.el ends here
--- dot-emacs/.emacs.d/lisp/init-magit.el
+++
@@ -1,8 +0,0 @@
-
-;; Magit git front-end
-(use-package magit
-  :defer t
-  :ensure t
-  :bind(("C-c g" . magit-status)))
-
-(provide 'init-magit)

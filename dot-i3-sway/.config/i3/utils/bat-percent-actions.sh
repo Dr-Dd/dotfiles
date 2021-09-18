404128a18de938bd54117ca9d27f3ff88d25ef9b
drd <idone.leonardo@gmail.com>
2021-09-18 20:00:18 +0200

    Fully removed every dependency except bash

--- dot-i3-sway/.config/i3/utils/bat-percent-actions.sh
+++ dot-i3-sway/.config/i3/utils/bat-percent-actions.sh
@@ -1,9 +1,4 @@
-#!/bin/sh
-
-# TODO: remove repeated code snippets.
-#	right now i didn't since creating a function that accepts a map as
-#	argument in bash is terribly unclean, so unclean that it's more
-#	easily readable to have long portions of code repeated.
+#!/bin/bash
 
 # AKA Suicide
 self_kill() {
@@ -21,6 +16,7 @@ _interval=
 _suffix=
 _prefix=
 _curr_perc=
+_regexp=
 
 # $1 -> action string
 action_str_to_vars () {
@@ -39,11 +35,13 @@ action_str_to_vars () {
 # $3 -> current percentage
 exec_action() {
 	# Strip correct stuff
-	_percent=`echo $1 | grep -Eo "[[:digit:]]*"`
+	_regexp='[[:digit:]]+'
+	[[ $1 =~ $_regexp ]] && _percent=${BASH_REMATCH[0]}
 	if [ -z "$_percent" ] ; then
 		self_kill "ERROR: missing percentage"
 	fi
-	_comparator=`echo $1 | grep -Eo "(>|>=|<|<=|=)"`
+	_regexp="(>|>=|<|<=|=)"
+	[[ $1 =~ $_regexp ]] && _comparator=${BASH_REMATCH[0]}
 	if [ -z "$_comparator" ] ; then
 		self_kill "ERROR: missing comparator operator"
 	fi
@@ -86,14 +84,13 @@ do
 		-i|--interval)
 			if [ "$2" ]; then
 				_interval="$2"
-				_suffix=`echo "$2" | sed "s/^[[:digit:]]*//"`
-				if [ -n "$_suffix" ] ; then
-					echo $_suffix | grep -Eo "^(s|m|h|d)$" > /dev/null 2>&1
-					if [ $? -eq 1 ] ; then
-						self_kill 'ERROR: invalid interval unit provided.'
-					fi
+				_regexp="[smhd]"
+				[[ $2 =~ $_regexp ]] && _suffix=${BASH_REMATCH[0]}
+				if [ -z "$_suffix" ] ; then
+					self_kill 'ERROR: invalid interval unit provided.'
 				fi
-				_prefix=`echo "$2" | grep -Eo "^[[:digit:]]*"`
+				_regexp="^[[:digit:]]*"
+				[[ $2 =~ $_regexp ]] && _prefix=${BASH_REMATCH[0]}
 				if [ -z "$_prefix" ] ; then
 					self_kill 'ERROR: invalid "--interval" provided.'
 				fi
@@ -117,18 +114,16 @@ fi
 while :
 do
 	# Refresh bat percentage
-	_curr_perc=`upower -i /org/freedesktop/UPower/devices/battery_BAT0 \
-		| grep percentage \
-		| grep -Eo "[[:digit:]]{,3}"`
-	_ac_state=`upower -i /org/freedesktop/UPower/devices/battery_BAT0 \
-		| grep state \
-		| grep -Eo "(charging|discharging)"`
-	if [ "$_ac_state" = "charging" ] ; then
+	_bat_amp=`cat /sys/class/power_supply/BAT0/charge_now`
+	_bat_amp_full=`cat /sys/class/power_supply/BAT0/charge_full`
+	_curr_perc=`printf %.0f "$((100 * $_bat_amp / $_bat_amp_full))"`
+	_ac_state=`cat /sys/class/power_supply/BAT0/status`
+	if [ "$_ac_state" = "Charging" ] ; then
 		for i in "${!_ac_action_map[@]}"
 		do
 			exec_action "$i" "${_ac_action_map[$i]}" "$_curr_perc"
 		done
-	elif [ "$_ac_state" = "discharging" ] ; then
+	elif [ "$_ac_state" = "Discharging" ] ; then
 		for i in "${!_battery_action_map[@]}"
 		do
 			exec_action "$i" "${_battery_action_map[$i]}" "$_curr_perc"
